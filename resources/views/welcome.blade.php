d25e948cb193b9c872706702a1a7d7a3c571e4a8
taroloaz6-web <tarolo.az6@gmail.com>
2025-09-22 12:25:43 +0200

    Revert "cfgg"
    This reverts commit 6d4ce66c8b3afc4a6d952c8593a8882c47fd9619.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-     <div>Szíílya, {{$name}}</div>
+     <div>Hi, {{$name}}</div>
        <div>Ez a nap van ma: {{ $day }}</div>
         <div>Szám: {{$number}} </div>
          @if ($logikai)
